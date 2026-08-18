82ac35df57ea5944718cbfea39c06dec956c9377
Jere Fetula <jf8726@edu.turku.fi>
2026-08-18 10:43:08 +0300

    Create aika.js

---
+++ aika.js
@@ -0,0 +1,15 @@
+function getCurrentDateTime() {
+  const now = new Date();
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+  };
+  return now.toLocaleString('en-US', options);
+}
+
+console.log(getCurrentDateTime());
